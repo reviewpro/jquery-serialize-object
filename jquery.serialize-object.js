5f067b906ad8ea715aad237f0c84fbb79adad97d
David Reyes <david.reyes@reviewpro.com>
2013-11-12 11:31:37 +0100

    Ignore array indexes
    If we have something like a[2] then 1 is not interpreted as the third
    element of an a array. It is interpreted as an a object with a '2' key.

--- jquery.serialize-object.js
+++ jquery.serialize-object.js
@@ -1,10 +1,3 @@
-/*!
- * jQuery serializeObject
- * http://github.com/macek/jquery-serialize-object
- *
- * Copyright 2013 Paul Macek <paulmacek@gmail.com>
- * Released under the BSD license
- */
 ;(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);throw new Error("Cannot find module '"+o+"'")}var f=n[o]={exports:{}};t[o][0].call(f.exports,function(e){var n=t[o][1][e];return s(n?n:e)},f,f.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 var FormSerializer = module.exports = function FormSerializer(helper) {
   this._helper    = helper;
@@ -14,7 +7,6 @@ var FormSerializer = module.exports = function FormSerializer(helper) {
     validate: /^[a-z][a-z0-9_]*(?:\[(?:\d*|[a-z0-9_]+)\])*$/i,
     key:      /[a-z0-9_]+|(?=\[\])/gi,
     push:     /^$/,
-    fixed:    /^\d+$/,
     named:    /^[a-z0-9_]+$/i
   };
 };
@@ -34,11 +26,6 @@ FormSerializer.prototype._makeObject = function _nest(root, value) {
     if (this._patterns.push.test(k)) {
       var idx = this._incrementPush(root.replace(/\[\]$/, ''));
       value = this._build([], idx, value);
-    }
-
-    // foo[n]
-    else if (this._patterns.fixed.test(k)) {
-      value = this._build([], k, value);
     }
 
     // foo; foo[bar]
@@ -73,7 +60,7 @@ FormSerializer.prototype.addPairs = function addPairs(pairs) {
 };
 
 FormSerializer.prototype.serialize = function serialize() {
-  return this._object
+  return this._object;
 };
 
 FormSerializer.prototype.serializeJSON = function serializeJSON() {
